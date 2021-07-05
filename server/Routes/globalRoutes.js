fa8438829468f196f5e3d2367f121dc7b32729ff
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-07-06 00:50:36 +0900

    logout revise2
    일부수정

--- server/Routes/globalRoutes.js
+++ server/Routes/globalRoutes.js
@@ -12,11 +12,13 @@ import{
 const globalRoutes = express.Router();
 globalRoutes.post(routes.register,postRegister);
 globalRoutes.post(routes.login,postLogin);
-globalRoutes.get(routes.logout,logout);
+globalRoutes.get(routes.logout,auth,logout);
 globalRoutes.get(routes.auth, auth,(req, res)=>{
     res.status(200).json({
+        user_id:req.user_id,
         isAdmin: req.user.role === 0 ? false :true,
         isAuth: true,
+        role:req.user.role
     })
 });
 
