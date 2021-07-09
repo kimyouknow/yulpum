a65e1bc9b0f0fa3a7682453d5fab64b1668529b6
philos1234 <48795102+philos1234@users.noreply.github.com>
2021-07-10 00:34:01 +0900

    subject Controller
    과목

--- server/Routes/globalRoutes.js
+++ server/Routes/globalRoutes.js
@@ -15,7 +15,7 @@ globalRoutes.post(routes.login,postLogin);
 globalRoutes.get(routes.logout,auth,logout);
 globalRoutes.get(routes.auth, auth,(req, res)=>{
     res.status(200).json({
-        user_id:req.user_id,
+        user_id:req.user._id,
         isAdmin: req.user.role === 0 ? false :true,
         isAuth: true,
         role:req.user.role
--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import {
+    saveStudy,
+    addSubject
+} from "../Controller/subjectController"
 
 const userRoutes = express.Router();
 
 userRoutes.post(routes.saveStudy,saveStudy);
-
+userRoutes.post(routes.addSubject,addSubject);
 
 export default userRoutes;
