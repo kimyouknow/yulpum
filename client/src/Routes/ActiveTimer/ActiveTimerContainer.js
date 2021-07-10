1cf77a5a8262583c5729b606a7bb160becfc4aa0
yunho <kimyouknow@naver.com>
2021-07-10 21:09:35 +0900

    update active router

--- client/src/Routes/ActiveTimer/ActiveTimerContainer.js
+++ client/src/Routes/ActiveTimer/ActiveTimerContainer.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from "react";
 import ActiveTimerPresenter from "./ActiveTimerPresenter";
 
-const ActiveTimerContainer = ({userID}) => {
-    console.log(userID);
+const ActiveTimerContainer = () => {
     return(
         <ActiveTimerPresenter />
         )
