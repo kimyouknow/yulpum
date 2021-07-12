d69eea339c151ca453ce7f409c4aa419015bcbba
yunho <kimyouknow@naver.com>
2021-07-12 22:59:15 +0900

    Merge branch 'client' into master1

--- client/src/_actions/types.js
+++ client/src/_actions/types.js
@@ -4,4 +4,5 @@ export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'auth_user';
 export const ADD_SUBJECT = 'add_subject';
 export const GET_SUBJECTS = 'get_subjects';
-export const TIMER = 'timer';
+export const UPDATE_TIMER = 'update_timer';
+export const GET_SUBJECT_TIME = 'get_subect_time';
