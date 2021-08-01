dc9755dfd61d15d910b9087a59ccd7b978c5f59b
yunho <kimyouknow@naver.com>
2021-08-01 22:27:51 +0900

    show todo in calendar

--- client/src/Routes/Stat/StatPresenter.js
+++ client/src/Routes/Stat/StatPresenter.js
@@ -51,9 +51,11 @@ const DateContainer = styled.div`
 
 const DateComponent = styled.div`
     display: flex;
-    justify-content:center;
-    align-items: center;
+    justify-content:flex-start;
+    align-items: flex-start;
+    padding: 8px;
     height: 70px;
+    position: relative;
     background-color: ${props => (props.timecolor < 0) ? "rgba(223, 230, 233,1.0)":
     (props.timecolor < 1000) ? "white" :
     (props.timecolor < 3000) ? "rgba(238, 90, 36, 0.3)" :
@@ -68,10 +70,35 @@ const DateComponent = styled.div`
     }
 `;
 
+const ToDoContainer = styled.div`
+    position: absolute;
+    bottom: 10px;
+    left: 0px;
+    width: 100%;
+    padding-left: 10px;
+    color: black;
+    &:hover {
+        background-color: rgba(223, 230, 233,1.0);
+        cursor: pointer;
+    }
+`;
+
 const TimeIndicatorContainer = styled.div`
     display: flex;
     justify-content:center;
     column-gap: 20px;
+    div:nth-child(1) div{
+        background-color: white;
+    }
+    div:nth-child(2) div{
+        background-color: rgba(238, 90, 36, 0.3);
+    }
+    div:nth-child(3) div{
+        background-color: rgba(238, 90, 36, 0.6);
+    }
+    div:nth-child(4) div{
+        background-color: rgba(238, 90, 36,1.0);
+    }
 `;
 
 const TimeIndicator = styled.div`
@@ -83,57 +110,45 @@ const TimeIndicatorColor = styled.div`
     width: 20px;
     border-radius: 50%;
     margin-right: 10px;
-    :nth-child(1){
-        background-color: rgba(223, 230, 233,1.0);
-    }
-    :nth-child(2n){
-        background-color: rgba(238, 90, 36, 0.3);
-    }
-    :nth-child(3){
-        background-color: rgba(238, 90, 36, 0.6);
-    }
-    :nth-child(4){
-        background-color: rgba(238, 90, 36,1.0);
-    }
 `;
 
-const StatPresenter = ({today,
-    weeks,
-    dates,
-    handleLastMonth,
-    handleNextMonth,
-    handleToday}) => {
-        return (
-            <Container>
-                    <Header>
-                        <Button onClick={() => handleLastMonth()}>◀</Button>
-                        <DisplayMonth onClick={()=> handleToday()}>{today}</DisplayMonth>
-                        <Button onClick={() => handleNextMonth()}>▶</Button>
-                    </Header>
-                    <WeekContainer>
-                        {weeks.map(week => <WeekComponent key={week}>{week}</WeekComponent>)}
-                    </WeekContainer>
-                    <DateContainer>
-                         {/* {console.log(dates)} */}
-                    {!dates ? <h1>Loading</h1> : 
-                    dates.map(date => <DateComponent key={date.date} timecolor={date.total_time} >{date.date.getDate()}</DateComponent>)}
-                    </DateContainer>
-                    <TimeIndicatorContainer>
-                        <TimeIndicator>
-                            <TimeIndicatorColor /> 0시간
-                        </TimeIndicator> 
-                        <TimeIndicator>
-                            <TimeIndicatorColor /> 0시간
-                        </TimeIndicator>
-                        <TimeIndicator>
-                            <TimeIndicatorColor /> 0시간
-                        </TimeIndicator>
-                        <TimeIndicator>
-                            <TimeIndicatorColor /> 0시간
-                        </TimeIndicator>
-                    </TimeIndicatorContainer>
-            </Container>
-        )
-    }
+const StatPresenter = ({today, weeks, dates, handleNextMonth, handleLastMonth, handleToday}) => {
+    return (
+        <Container>
+                <Header>
+                    <Button onClick={() => handleLastMonth()}>◀</Button>
+                    <DisplayMonth onClick={()=> handleToday()}>{today}</DisplayMonth>
+                    <Button onClick={() => handleNextMonth()}>▶</Button>
+                </Header>
+                <WeekContainer>
+                    {weeks.map(week => <WeekComponent key={week}>{week}</WeekComponent>)}
+                </WeekContainer>
+                <DateContainer>
+                        {/* {console.log(dates)} */}
+                {!dates ? <h1>Loading</h1> : 
+                dates.map(date =>
+                    <DateComponent key={date.date} timecolor={date.total_time} >
+                        {date.date.getDate()}
+                        <ToDoContainer>{date.todo}</ToDoContainer>
+                    </DateComponent>)
+                }
+                </DateContainer>
+                <TimeIndicatorContainer>
+                    <TimeIndicator>
+                        <TimeIndicatorColor /> 0~3 시간
+                    </TimeIndicator> 
+                    <TimeIndicator>
+                        <TimeIndicatorColor /> 3~6 시간
+                    </TimeIndicator>
+                    <TimeIndicator>
+                        <TimeIndicatorColor /> 6~9 시간
+                    </TimeIndicator>
+                    <TimeIndicator>
+                        <TimeIndicatorColor /> 9 시간 이상
+                    </TimeIndicator>
+                </TimeIndicatorContainer>
+        </Container>
+    )
+}
 
 export default StatPresenter
