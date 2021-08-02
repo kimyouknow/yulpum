cac31fd871e2b777fbb127e3b6cffc2b1cced89e
yunho <kimyouknow@naver.com>
2021-08-02 22:04:19 +0900

    Revert "Update StatContainer.js"
    This reverts commit df06042d0bd74788aec7586af7912cae3f0cf65a.

--- client/src/Routes/Stat/StatContainer.js
+++ client/src/Routes/Stat/StatContainer.js
@@ -16,11 +16,11 @@ const StatContainer = () => {
         const renderMonth = dato.getMonth();
         setToday(`${renderYear}년 ${renderMonth+1}월`);
         let body = {
-            year: Number(renderYear),
-            month: Number(renderMonth+1),
+            year: renderYear,
+            month: renderMonth,
             token: tokenData
         }
-        // console.log(body);
+
         dispatch(getCalendar(body))
             .then(response => {
                 console.log(response)
