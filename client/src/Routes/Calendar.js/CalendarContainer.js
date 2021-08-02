9e496ee6b8fa75e8f630d403016f1190e8d371de
yunho <kimyouknow@naver.com>
2021-08-03 00:03:30 +0900

    update Calendar component

---
+++ client/src/Routes/Calendar.js/CalendarContainer.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react";
+import CalendarPresenter from "./CalendarPresenter";
+
+const CalendarContainer = () => {
+    return(
+        <CalendarPresenter />
+        )
+}
+export default CalendarContainer
--- client/src/Routes/Timer.js/TimerPresenter.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const TimerPresenter = () => (
-    <>
-        <h1>Timer</h1>
-    </>
-)
-export default TimerPresenter
