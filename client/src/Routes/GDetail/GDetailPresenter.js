c60cfe8ed6299e4a0134bfcade15267394111491
yunho <kimyouknow@naver.com>
2021-08-27 22:31:56 +0900

    update exit group

--- client/src/Routes/GDetail/GDetailPresenter.js
+++ client/src/Routes/GDetail/GDetailPresenter.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Body from '../../Styled/Body'
+import Container from '../../Styled/Container'
+import Header from '../../Styled/Header'
+import { SideBar } from '../../Styled/SideBar'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-function GDetailPresenter() {
+function GDetailPresenter({handleExit}) {
   return (
-    <div>
-      GDEtail
-    </div>
+    <Container>
+      <Header>??</Header>
+      <Body>
+        <SideBar>
+          <div className={"subBtn first empty"}></div>
+          <div className={"subBtn second"} onClick={()=> handleExit()}>
+              그룹 탈퇴
+          </div>
+          <div className={"menuBtn"}>
+              <FontAwesomeIcon icon={faBars} />
+          </div>
+        </SideBar>
+      </Body>
+    </Container>
   )
 }
 
